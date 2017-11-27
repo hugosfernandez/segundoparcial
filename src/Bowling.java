3a80ef804cc3cb80072246ffa4596338d56f419a
hugosfernandez <hugofer_007@hotmail.com>
2017-11-27 16:56:04 -0300

    no existe clase Bowlinggame

---
+++ src/Bowling.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Bowling {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testcrearjuego() {
+		Bowlinggame game = new Bowlinggame;
+	}
+
+}
