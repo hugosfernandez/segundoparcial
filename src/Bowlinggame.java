469469de6bf792262ac89d568adde2f7d82466e1
hugosfernandez <hugofer_007@hotmail.com>
2017-11-27 17:36:52 -0300

    verde testmejorpartida

--- src/Bowlinggame.java
+++ src/Bowlinggame.java
@@ -3,13 +3,15 @@ public class Bowlinggame {
 
 	//public static final String Puntaje = null;
 
+	private int puntaje;
+	
 	public int Puntaje() {
-		return 0;
+		return puntaje;
 	} 
 	
 	public void Rollframe(int i, int j) {
 		// TODO Auto-generated method stub
-		
+		puntaje += i + j;
 	}
 
 }
