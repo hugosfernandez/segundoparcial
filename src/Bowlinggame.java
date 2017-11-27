efaf3920c8b63597ac26e6c959f121a89ee127f9
hugosfernandez <hugofer_007@hotmail.com>
2017-11-27 17:38:14 -0300

    verde testmejorpartida refactorizacion

--- src/Bowlinggame.java
+++ src/Bowlinggame.java
@@ -9,9 +9,9 @@ public class Bowlinggame {
 		return puntaje;
 	} 
 	
-	public void Rollframe(int i, int j) {
+	public void Rollframe(int roll1, int roll2) {
 		// TODO Auto-generated method stub
-		puntaje += i + j;
+		puntaje += roll1 + roll2;
 	}
 
 }
